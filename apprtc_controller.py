5e2f67c97f53ca4cd3e8a239cd7ab65cb7b00403
Howard Hsu <howard.hsu1129@gmail.com>
2021-12-07 16:48:30 +0800

    fix minor bug

--- apprtc_controller.py
+++ apprtc_controller.py
@@ -2,6 +2,7 @@ import time
 import random
 from decouple import config
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
@@ -9,7 +10,7 @@ from selenium.webdriver.common.by import By
 class ApprtcController:
     browser = None
     room_id = None
-    chromedriver_path = config('DRIVERPATH', default='/usr/lib/chromium-browser/chromedriver')
+    chromedriver = Service(config('DRIVERPATH', default='/usr/lib/chromium-browser/chromedriver'))
 
     @classmethod
     def start(cls):
@@ -19,7 +20,7 @@ class ApprtcController:
             chrome_options.add_argument('use-fake-ui-for-media-stream')
 
             cls.room_id = str(random.randint(100000000, 999999999))
-            cls.browser = webdriver.Chrome(executable_path=cls.chromedriver_path, options=chrome_options)
+            cls.browser = webdriver.Chrome(service=cls.chromedriver, options=chrome_options)
             cls.browser.get(f'https://appr.tc/r/{cls.room_id}')
             join_button = cls.browser.find_element(By.ID, 'confirm-join-button')
             join_button.click()
