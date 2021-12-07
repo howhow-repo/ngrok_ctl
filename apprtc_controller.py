c3cc0da4486a65478e578074e0ca31dc01048052
Howard Hsu <howard.hsu1129@gmail.com>
2021-12-07 10:00:41 +0800

    clean code

--- apprtc_controller.py
+++ apprtc_controller.py
@@ -1,27 +1,27 @@
-import os
-import random
 import time
-
+import random
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 class ApprtcController:
     browser = None
     room_id = None
-    chromedriver = '/usr/lib/chromium-browser/chromedriver'
+    chromedriver = Service('/usr/lib/chromium-browser/chromedriver')
 
     @classmethod
     def start(cls):
-        if cls.room_id is None:
+        if cls.browser is None:
             chrome_options = Options()
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('use-fake-ui-for-media-stream')
 
             cls.room_id = str(random.randint(100000000, 999999999))
-            cls.browser = webdriver.Chrome(executable_path=cls.chromedriver, options=chrome_options)
+            cls.browser = webdriver.Chrome(service=cls.chromedriver, options=chrome_options)
             cls.browser.get(f'https://appr.tc/r/{cls.room_id}')
-            join_button = cls.browser.find_element('confirm-join-button')
+            join_button = cls.browser.find_element(By.ID, 'confirm-join-button')
             join_button.click()
 
         return cls.room_id
@@ -34,6 +34,7 @@ class ApprtcController:
 
 
 if __name__ == '__main__':
-    ApprtcController.start()
+    room_id = ApprtcController.start()
+    print(room_id)
     time.sleep(10)
     ApprtcController.stop()
