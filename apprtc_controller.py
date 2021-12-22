94117bb509630c01d799af6ac22f723896cde028
Howard Hsu <howard.hsu1129@gmail.com>
2021-12-22 17:21:09 +0800

    change apprtc site to talky.io; use join button

--- apprtc_controller.py
+++ apprtc_controller.py
@@ -1,13 +1,15 @@
 import logging
+import time
 import random
 from decouple import config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
 
 logger = logging.getLogger(__name__)
+
 
 class ApprtcController:
     browser = None
@@ -18,16 +20,33 @@ class ApprtcController:
     def start(cls):
         if cls.browser is None:
             chrome_options = Options()
-            chrome_options.add_argument('--headless')  # 無視窗
-            chrome_options.add_argument('use-fake-ui-for-media-stream')
-            chrome_options.add_argument('--incognito')  # 無痕
-            chrome_options.add_argument('--no-sandbox')  # 解決DevToolsActivePort檔案不存在的報錯
+            # chrome_options.add_argument('--headless')  # 無視窗
+            chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+            chrome_options.add_argument('--use-fake-ui-for-media-stream')
+            # chrome_options.add_argument('--incognito')  # 無痕
+            chrome_options.add_argument('--no-sandbox')  # DevToolsActivePort
+            # chrome_options.add_argument('--disable-gpu')
+
             cls.room_id = str(random.randint(100000000, 999999999))
             cls.browser = webdriver.Chrome(service=cls.chromedriver, options=chrome_options)
+
             try:
-                cls.browser.get(f'https://talk.io/{cls.room_id}')
-                # join_button = cls.browser.find_element(By.ID, 'confirm-join-button')
-                # join_button.click()
+                cls.browser.get(f'https://talky.io/{cls.room_id}')
+                time.sleep(10)
+                ele_select = cls.browser.find_element(By.XPATH,
+                                                      "/html/body/div[@id='root']"
+                                                      "/div[1]/div[1]/div[1]/div[2]/div[3]/div[2]/label/select")
+
+                selects = Select(ele_select)
+                selects.select_by_index(0)
+
+                time.sleep(2)
+
+                join_button = cls.browser.find_element(By.XPATH,
+                                                       "/html/body/div[@id='root']"
+                                                       "/div[1]/div[1]/div[1]/div[2]/div[3]/div[4]/button")
+                join_button.click()
+
             except Exception as e:
                 logger.error(e)
                 cls.stop()
